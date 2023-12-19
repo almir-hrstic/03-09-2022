1da6d8bf8aa81dc17f090a8b6cd61de9ebc84e7d
Almir Hrštić <almirhrstic@gmail.com>
2023-12-19 13:05:12 +0100

    19.12.2023

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ export default function Page() {
 
   let imageInterval = null
 
-  const setScreenHeight = () => root.current.style.setProperty('height', `${window.innerHeight - .5}px`)
+  const setScreenHeight = () => root.current.style.setProperty('max-height', `${window.innerHeight}px`)
 
   const setImages = () => {
 
